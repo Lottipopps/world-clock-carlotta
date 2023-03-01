360a4d446da85c52f48bc082621776f043cbf3ba
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-03-01 18:40:27 +0100

    Moved my HTML into JS and added real time clocks to the cities

---
+++ src/app.js
@@ -0,0 +1,24 @@
+function showCities() {
+  let citiesElement = document.querySelector("#cities");
+  let cities = [
+    "Europe/Bologna",
+    "America/New_York",
+    "America/Mexico_City",
+    "Australia/Sydney",
+  ];
+  citiesElement.innerHTML = cities
+    .map(function showCity(city) {
+      return `<div class="city" id="${city}">
+          <div class="date-place">
+            <h2>${city.substring(city.indexOf("/") + 1).replace("_", " ")}</h2>
+            <div class="date">${moment
+              .tz(city)
+              .format("dddd Do MMMM YYYY")}</div>
+          </div>
+          <div class="time">${moment.tz(city).format("HH:mm:ss")}</div>
+        </div>`;
+    })
+    .join("");
+}
+showCities();
+setInterval(showCities, 1000);
